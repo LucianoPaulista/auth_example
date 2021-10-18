8a60a35180116deea81f0e04be964aeab60548a0
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-10-18 17:04:31 -0300

    feat: commit geral

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
-company = Company.create(description: "teste")
+#company = Company.create(description: "teste")
+
+Role.create(name: "company.update", entity_id: 1)
+Assignment.create(role_id: 1, user_id: 1)
+Assignment.create(role_id: 2, user_id: 1)
+
+
+
 
 
